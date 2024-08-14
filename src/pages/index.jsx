ec71762535fab1ec3808f7c950fcde43eaa6f13e
Vin R. <vramroop6@gmail.com>
2024-08-14 08:37:04 -0400

    m

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,28 +18,39 @@ const Home = () => {
   // <Page>
     <div className={`flex flex-col min-h-screen ${isDarkMode ? 'dark bg-white' : 'bg-gradient-to-b from-blue-400 to-blue-600'}`}>
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <title>Studiously</title>
+      <title>Home</title>
       <Analytics />
       <Navbar />
-      <Clock format={'h:mm:ssa'} style={{ fontSize: '1.5em' }} ticking={true} className='text-white text-center my-2' />
-      <div class="shape-blob"></div>
+     <div class="shape-blob"></div>
       <div class="shape-blob one"></div>
       <div class="shape-blob two"></div>
       <h1 className={`text-5xl mb-4 font-bold text-center my-6 py-6 ${isDarkMode ? 'text-black' : 'text-white'}`}>
-        Welcome to Studiously
+        3888 Greased Lightning
       </h1>
+
       <div className="flex-grow flex flex-col items-center justify-center">
-        <div className='flex gap-4'>
+        <div className='flex gap-2'>
           <div className={`border border-blue-300 rounded-lg p-4 shadow-md text-center text-xl ${isDarkMode ? 'text-black' : 'text-white'}`}>
-            <p className='text-lg'>Your all-in-one homework browser.</p>
-            <img src="https://github.com/vintheruler1/Studiously/blob/main/public/studiously.png?raw=true" width={280} height={280} alt="logo" />
+            <p className='text-lg'>Located in Weston, Florida. We are a FIRST (rights reserved) Tech Challenge (add wtv) Team! We compete in the South Florida Gator Bots League.</p>
           </div>
         </div>
       </div>
+
+      <h2 className={`text-3xl font-bold text-center py-6 ${isDarkMode ? 'text-black' : 'text-white'}`}>
+        Latest Robot
+      </h2>
+
+      <div className="flex-grow flex flex-col items-center justify-center">
+        <div className='flex gap-2'>
+          <p className='text-lg text-center text-white'>Our newest robot, "Bad Decisions" is a off season robot used to prototype South Floridas <b>first</b> swerve drive.</p>
+        </div>
+      </div>
+
+      
+      
+
       <footer className={`text-sm align-middle text-center text-white`}>
-        &copy; 2024 | 3888 Greased Lightning |{' '}
-        <a href="/AboutUs" className="text-white hover:underline">About Us</a> |{' '}
-        <a href="/terms-and-conditions" className="text-white hover:underline">Terms and Conditions</a>
+        &copy; 3888 Greased Lightning 
       </footer>
     </div>
   // </Page>
